e0703b9fbb60422f08629cfbba6e4daf42be8734
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2023-08-28 16:27:22 +0000

    simpleCounterRealizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,22 +2,13 @@ import React from "react";
 import SecondsCounter from "../secondscounter.jsx";
 
 
-
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = (props) => {
-	//console.log(props)
 	return (
-		<div className="text-center ">
+		<>
+		<div>
 		<SecondsCounter n1={props.n1} n2={props.n2} n3={props.n3} n4={props.n4} n5={props.n5} n6={props.n6}/>
-		
-
-			
 		</div>
-
+		</>
 	);
 };
 
--- src/js/secondscounter.jsx
+++ src/js/secondscounter.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { stopInterval, continueInterval,pauseInterval, alternateMode } from "./index.js";
+import time from "../img/time.png";
+import "../styles/secondscounter.css";
+
+
 let status = true;
 
 const SecondsCounter = (props) => {
   console.log(props.n1);
    return ( 
 
-   <div className="container text-center" style={{ height:"100px", width:"800px",backgroundColor:"black",borderRadius:"20px"}}>
+   <div className="container text-center" style={{ height:"100px", width:"800px",backgroundColor:"#8f6d11",borderRadius:"20px"}}>
    <div className="row row-cols-7" style={{marginBottom:"30px",color:"white", marginTop:"50px"}}>
-     <div className="col" style={{fontSize:"60px"}}>🕒</div>
+     <div className="col" style={{fontSize:"60px"}}>⏳</div>
      <div className="col" style={{fontSize:"60px"}}>{props.n6}</div>
      <div className="col" style={{fontSize:"60px"}}>{props.n5}</div>
      <div className="col" style={{fontSize:"60px"}}>{props.n4}</div>
@@ -17,13 +21,14 @@ const SecondsCounter = (props) => {
      <div className="col" style={{fontSize:"60px"}}>{props.n1}</div>
    </div>
 
-   <button onClick={stopInterval} style={{backgroundColor:"#7BE38B",borderRadius:"10px",color:"#434646",marginRight:"5px"}}><b>Restart</b></button>
-   <button onClick={continueInterval} style={{backgroundColor:"#F1F16F",borderRadius:"10px",color:"#434646",marginRight:"5px"}}><b>Continue</b></button>
-   <button onClick={pauseInterval} style={{backgroundColor:"#F35A48",borderRadius:"10px",color:"#434646",marginRight:"5px"}}><b>Pause</b></button>
-   <button onClick={alternateMode} style={{backgroundColor:"#70DCDB ",borderRadius:"10px",color:"#434646"}}> <b>Alternate</b> </button>
-   
+   <div>
+     <img src={time}/>
+   </div>
 
-
+   <button className="restart" onClick={stopInterval} style={{backgroundColor:"#619343",borderRadius:"10px",marginRight:"30px"}}><b>Restart</b></button>
+   <button className="continue" onClick={continueInterval} style={{backgroundColor:"#FFC930",borderRadius:"10px",marginRight:"30px"}}><b>Continue</b></button>
+   <button className="pause" onClick={pauseInterval} style={{backgroundColor:"#852929",borderRadius:"10px",marginRight:"30px"}}><b>Pause</b></button>
+   <button className="regressive" onClick={alternateMode} style={{backgroundColor:"#70DCDB ",borderRadius:"10px"}}> <b>Regressive</b> </button>
    </div>
     );
 };
